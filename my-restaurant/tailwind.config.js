bafe1c373a5015d07ddbe9b27fa18b117e26bad3
김우진 <kedw158@gmail.com>
2026-02-04 13:25:01 +0900

    docs: 코멘트 확인 후 수정 완료

--- my-restaurant/tailwind.config.js
+++ my-restaurant/tailwind.config.js
@@ -1,1 +1,8 @@
-@import "tailwindcss";
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
