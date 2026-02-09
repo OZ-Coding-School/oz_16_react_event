a0cb403c3f2518e8e4636c2ffb5352bd33b82b00
김우진 <kedw158@gmail.com>
2026-02-09 10:27:41 +0900

    feat: HTTP 404 에러 처리 및 내 위치 기반 맛집 정렬 기능 구현

--- my-restaurant/src/api/restaurant.js
+++ my-restaurant/src/api/restaurant.js
@@ -1,18 +1,34 @@
 export const BASE_URL = "http://localhost:3000";
 
-// 전체 맛집
-export const fetchPlaces = async () => {
-  try {
-    const response = await fetch(`${BASE_URL}/places`);
+// 맛집 정보를 가져오는 함수
+export async function fetchPlaces() {
+  const response = await fetch(`${BASE_URL}/places`);
 
-    if (!response.ok) {
-      throw new Error("데이터 로드 실패");
+  // 서버 응답이 실패했을 때 에러 처리
+  if (!response.ok) {
+    if (response.status === 404) {
+      throw new Error("요청하신 페이지를 찾을 수 없어요. (404)");
     }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error(error);
-    return [];
+    throw new Error("데이터를 불러오는 데 실패했습니다.");
   }
-};
+
+  const data = await response.json();
+  return data;
+}
+
+export async function fetchUserPlaces() {
+  const response = await fetch(`${BASE_URL}/users/places`);
+  if (!response.ok) throw new Error("찜 목록을 가져오지 못했습니다.");
+  return await response.json();
+}
+
+// 찜한 목록
+export async function updateUserPlaces(places) {
+  const response = await fetch(`${BASE_URL}/users/places`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ places }),
+  });
+  if (!response.ok) throw new Error("서버 저장에 실패했습니다.");
+  return await response.json();
+}
